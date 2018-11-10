7d41adb0cc5711532a60228f14946dfe6ae49f00
ahmed8899 <43449958+ahmed8899@users.noreply.github.com>
2018-11-09 23:42:55 -0800

    Update hilton.js

--- hilton.js
+++ hilton.js
@@ -12,7 +12,8 @@ function Hotel(name, rooms, booked) {
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-var hiltonHotel = new Hotel ('Hilton', 86, 10);
+var hiltonHotel = new Hotel('hilton', 86, 10);
+
 
 // Update the HTML for the page
 var details1 = quayHotel.name + ' rooms: ';
@@ -25,11 +26,10 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
-var details3 = HiltonHotel.name + ' rooms: ';
-    details3 += HiltonHotel.checkAvailability();
+var details3 = hiltonHotel.name + ' rooms: ';
+    details3 += hiltonHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details3;
-
 /* 
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
